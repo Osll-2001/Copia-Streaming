d68e741caf7c40b7bc99c12eea82dea45eb25f34
octavio <octaviosuriellopezlopez@gmail.com>
2022-04-02 19:38:36 -0600

    Proyecto Copia Streaming v.1

---
+++ script.js
@@ -0,0 +1,9 @@
+//FILTRO DE DESENFOQUE A CATEGORIAS
+$( ".cat_familia" ).mouseover(function() {$('.img_familia').css('filter','blur(4px)' );}); $( ".cat_familia" ).mouseleave(function() {$('.img_familia').css('filter','blur(0px)' );});
+$(".cat_fantasia").mouseover(function() {$('.img_fantasia').css('filter','blur(4px)' );}); $( ".cat_fantasia" ).mouseleave(function() {$('.img_fantasia').css('filter','blur(0px)' );});
+$(".cat_foreign").mouseover(function() {$('.img_foreign').css('filter','blur(4px)' );}); $( ".cat_foreign" ).mouseleave(function() {$('.img_foreign').css('filter','blur(0px)' );});
+$(".cat_guerra").mouseover(function() {$('.img_guerra').css('filter','blur(4px)' );}); $( ".cat_guerra" ).mouseleave(function() {$('.img_guerra').css('filter','blur(0px)' );});
+$(".cat_historia").mouseover(function() {$('.img_historia').css('filter','blur(4px)' );}); $( ".cat_historia" ).mouseleave(function() {$('.img_historia').css('filter','blur(0px)' );});
+$(".cat_misterio").mouseover(function() {$('.img_misterio').css('filter','blur(4px)' );}); $( ".cat_misterio" ).mouseleave(function() {$('.img_misterio').css('filter','blur(0px)' );});
+$(".cat_crimen").mouseover(function() {$('.img_crimen').css('filter','blur(4px)' );}); $( ".cat_crimen" ).mouseleave(function() {$('.img_crimen').css('filter','blur(0px)' );});
+$(".cat_documental").mouseover(function() {$('.img_documental').css('filter','blur(4px)' );}); $( ".cat_documental" ).mouseleave(function() {$('.img_documental').css('filter','blur(0px)' );});
